d407c7b55cc422e24187e049e24357cbe9e685ad
Ajay7913 <ajayware79@gmail.com>
2025-08-22 13:06:00 +0530

    Initial Commit

---
+++ src/Employee/Developer.java
@@ -0,0 +1,17 @@
+package Employee;
+
+public class Developer extends Employee {
+
+    private String programmingLanguage;
+
+    public Developer(String name, String department, double salary, String programmingLanguage) {
+        super(name, department, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ajay
+ *
+ */
+module Employee_managment_System {
+}
